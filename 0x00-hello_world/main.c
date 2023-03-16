513d00cb244e29a7ccba4f67d1ebf9bf487ea7cd
saaid <saaid2538@gmail.com>
2023-03-16 07:53:14 -0700

    added 0-preprocessor

--- 0x00-hello_world/main.c
+++ 0x00-hello_world/main.c
@@ -1,15 +1,11 @@
-#include<stdio.h>
+#include <stdio.h>
 
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-
-	/*This function reuturns Hello World*/
-	printf("Hell World");
 	return (0);
 }
