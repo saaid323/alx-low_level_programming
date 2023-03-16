7c838f9d71beb40e6709ffa7891c224cb74a09ac
saaid <saaid2538@gmail.com>
2023-03-16 13:23:38 -0700

    added 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,6 +7,10 @@
  */
 int main(void)
 {
-	puts("size of int is: %d%", sizeof(int));
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("size of an int: %d  byte(s)\n", sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(lon long int));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
 	return (0);
 }
