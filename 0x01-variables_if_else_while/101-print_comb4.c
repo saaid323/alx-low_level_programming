71dc3f7eec98043833939a7b16f9faa7db2257f7
saaid <saaid2538@gmail.com>
2023-03-17 13:23:28 -0700

    added 101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, j, x;
+
+	for (i = 0; i <= 7; i++)
+	{
+		for (j = i + 1; j <= 8; j++)
+		{
+			for (x = j + 1; x <= 9; x++)
+			{
+				putchar(i + '0');
+				putchar(j + '0');
+				putchar(x + '0');
+				if (i <= 7 && j <= 8 && x <= 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
