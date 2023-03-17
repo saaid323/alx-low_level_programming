14ceda9243e6ae47b198b0aafba41a4bcea98b0a
saaid <saaid2538@gmail.com>
2023-03-17 13:50:03 -0700

    added 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,13 +9,16 @@ int main(void)
 {
 	int i, j;
 
-	for (i = 0; i <= 9; i++)
+	for (i = 0; i < 100; i++)
 	{
-		for (j = 0; j <= 9; j++)
+		for (j = 0; j < 100; j++)
 		{
 			putchar(i + '0');
+			putchar(i + '0');
+			putchar(' ');
 			putchar(j + '0');
-			if (i == 9 && j == 9)
+			putchar(j + '0');
+			if (i == 98 && j == 99)
 			{
 				putchar('\n');
 			}
