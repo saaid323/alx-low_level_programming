925ee2fbaf70d9ee3ca863a0ca0faa3b53ff203e
saaid <saaid2538@gmail.com>
2023-03-17 06:02:36 -0700

    added 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	char c;
+
+	for (i = 97; i <= 122; i++)
+	{
+		c = i;
+		putchar(c);
+	}
+	return (0);
+}
