981c001eb4d8399b24591be771752d1ae36ed4b1
saaid <saaid2538@gmail.com>
2023-03-17 13:25:44 -0700

    added 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -18,7 +18,11 @@ int main(void)
 				putchar(i + '0');
 				putchar(j + '0');
 				putchar(x + '0');
-				if (i <= 7 && j <= 8 && x <= 9)
+				if (i == 7 && j == 8 && x == 9)
+				{
+					putchar('\n');
+				}
+				else
 				{
 					putchar(',');
 					putchar(' ');
