bfdb07451735ea4acf87160b2f2965878164f181
saaid <saaid2538@gmail.com>
2023-03-21 08:25:01 -0700

    added 10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,11 +2,12 @@
 
 /**
  * add - Adda two numbers
- * @num1, @num2: numbers that are being added
+ * @num1: first number
+ * @num2: secomd number
  * Return: 0
  */
 
 int add(int num1, int num2)
 {
-	return num1 + num2;
+	return (num1 + num2);
 }
