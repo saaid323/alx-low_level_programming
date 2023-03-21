e31272d4d86e83806fb8fe3a6b6fe9f758a08295
saaid <saaid2538@gmail.com>
2023-03-21 14:15:23 -0700

    added 102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entery point
+ * Return: 0 Always
+ */
+
+int main(void)
+{
+	for (i = 0; a <= 50; i++)
+	{
+		printf("%d ", a);
+		b = a + b;
+		a = b - a;
+	}
+	return (0);
+}
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,21 +9,17 @@ void times_table(void)
 {
 	int i, j, result;
 
-	for (i = 0; i <= 10; i++)
-	{
-		_putchar('0');
-		_putchar(' ');
-	}
-	_putchar('\n');
+	
 	for (i = 1; i <= 9; i++)
 	{
 		_putchar('0');
+                _putchar(',');
 		_putchar(' ');
 		for (j = 1; j <= 9; j++)
 		{
 			result = j * i;
 
-			if (result >= 10)
+			if (result > 10)
 			{
 				_putchar((result / 10) + '0');
 				_putchar((result % 10) + '0');
