62307e1cb382128735a8625082497bfb8564d14e
saaid <saaid2538@gmail.com>
2023-03-21 11:58:38 -0700

    added 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -12,18 +12,18 @@ void print_times_table(int x)
 
 	for (k = 0; k <= x; k++)
 	{
-		printf("0 ");
+		printf("0   ");
 	}
 	printf("\n");
 	for (i = 1; i <= x; i++)
 	{
-		printf("0 ");
+		printf("0  ");
 		for (j = 1; j <= x; j++)
 		{
 			printf("%d", (i * j));
 			if (j < x)
 			{
-				printf(", ");
+				printf(",  ");
 			}
 		}
 		printf("\n");
