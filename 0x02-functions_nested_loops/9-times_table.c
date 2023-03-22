a71b77b390f1ee0a0c440ff5c4125e6179007721
saaid <saaid2538@gmail.com>
2023-03-22 01:20:47 -0700

    added 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,39 +8,46 @@
 void times_table(void)
 {
 	int i, j, result;
-
+	for (i = 1; i <= 10; i++)
+	{
+		_putchar(0 + '0');
+		if (i < 10)
+		{
+			_putchar(',');
+			_putchar(' ');
+			_putchar(' ');
+		}
+	}
+	_putchar('\n');
 	for (i = 1; i <= 9; i++)
-        {
-            _putchar(0 + '0');
-            _putchar(',');
-            _putchar(' ');
-            for (j = 1; j <= 9; j++)
-            {
-                result = i * j;
-                
-                if (result <= 9)
-                {
-                    _putchar(result % 10 + '0');
-                    if (j < 9)
-                    {
-                        _putchar(',');
-                        _putchar(' ');
-                        _putchar(' ');
-                    }
-                }
-                else
-                {
-                    _putchar(result / 10 + '0');
-                    _putchar(result % 10 + '0');
-                    if (j < 9)
-                    {
-                        _putchar(',');
-                        _putchar(' ');
-                    }
-                }
-                
-                
-            }
-            _putchar('\n');
-        }
+	{
+		_putchar(0 + '0');
+		_putchar(',');
+		_putchar(' ');
+		for (j = 1; j <= 9; j++)
+		{
+			result = i * j;
+			if (result <= 9)
+			{
+				_putchar(result % 10 + '0');
+				if (j < 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+				}
+			}
+			else
+			{
+				_putchar(result / 10 + '0');
+				_putchar(result % 10 + '0');
+				if (j < 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+		}
+		_putchar('\n');
+	}
 }
