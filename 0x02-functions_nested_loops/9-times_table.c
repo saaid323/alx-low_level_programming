9396443283ed15cae9e62a4c3189a67da945057e
saaid <saaid2538@gmail.com>
2023-03-22 02:31:38 -0700

    added 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -22,31 +22,25 @@ void times_table(void)
 	for (i = 1; i <= 9; i++)
 	{
 		_putchar(0 + '0');
-		_putchar(',');
-		_putchar(' ');
-		_putchar(' ');
 		for (j = 1; j <= 9; j++)
 		{
 			result = i * j;
-			if (result <= 9)
+			if (result < 10)
 			{
-				_putchar(result % 10 + '0');
-				if (j < 9)
+				if (j <= 9)
 				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
 				}
+				_putchar(result % 10 + '0');
 			}
 			else
 			{
+				_putchar(',');
+				_putchar(' ');
 				_putchar(result / 10 + '0');
 				_putchar(result % 10 + '0');
-				if (j < 9)
-				{
-					_putchar(',');
-					_putchar(' ');
-				}
 			}
 		}
 		_putchar('\n');
