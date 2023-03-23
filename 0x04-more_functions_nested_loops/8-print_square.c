e35a43ce50b326e63bd749564224a10ebbba0eb8
saaid <saaid2538@gmail.com>
2023-03-23 05:22:14 -0700

    added 01-print_number.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square
+ * @size: integer argument
+ */
+
+
+void print_square(int size)
+{
+	int i, j;
+
+	if (size < 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 1; i <= size; i++)
+		{
+			if (size > 0)
+			{
+				for (j = 1; j <= size; j++)
+				{
+					_putchar('#');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
