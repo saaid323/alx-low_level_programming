499a246c7372aef320dccbe6419bcff960406010
saaid <saaid2538@gmail.com>
2023-03-23 00:42:58 -0700

    added 0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * _isupper -  checks for uppercase character
+ * @c: integer argument
+ * Return: 1 if c is upper case
+ */
+
+
+int _isupper(int c)
+{
+	if (c >= 65 &&  c <= 91)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
