bc79b9a959eecd023cb1a6ba0c5512d0697fb0fa
saaid <saaid2538@gmail.com>
2023-03-23 02:18:03 -0700

    added 7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_diagonal - aws a diagonal line on the terminal
+ * @n: int argument
+ */
+
+void print_diagonal(int n)
+{
+    int i, j;
+
+    if (n <= 0) 
+    {
+        _putchar('\n');
+        return;
+    }
+
+    for (i = 1; i <= n; i++) 
+    {
+        for (j = 1; j <= i; j++)
+	{ 	
+            if (j == i) 
+	    {
+                _putchar('\\');
+                _putchar('\n');
+            }
+            else 
+	    {
+                _putchar(' ');
+            }
+        }
+    }
+}
+
