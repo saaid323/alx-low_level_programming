162c00215dbd5d1036a69d2a426357e162ad9133
saaid <saaid2538@gmail.com>
2023-03-23 02:30:12 -0700

    added 7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,28 +7,27 @@
 
 void print_diagonal(int n)
 {
-    int i, j;
+	int i, j;
 
-    if (n <= 0) 
-    {
-        _putchar('\n');
-        return;
-    }
-
-    for (i = 1; i <= n; i++) 
-    {
-        for (j = 1; j <= i; j++)
-	{ 	
-            if (j == i) 
-	    {
-                _putchar('\\');
-                _putchar('\n');
-            }
-            else 
-	    {
-                _putchar(' ');
-            }
-        }
-    }
+	if (n <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	for (i = 1; i <= n; i++)
+	{
+		for (j = 1; j <= i; j++)
+		{
+			if (j == i)
+			{
+				_putchar('\\');
+				_putchar('\n');
+			}
+			else
+			{
+				_putchar(' ');
+			}
+		}
+	}
 }
 
