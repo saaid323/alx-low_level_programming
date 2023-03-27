db9b983f587422a7d27309a392d915a1d7f2022f
saaid <saaid2538@gmail.com>
2023-03-27 07:58:36 -0700

    added 5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * rev_string - reverses a string
+ * @s: pointer to a string
+ */
+
+void rev_string(char *s)
+{
+	int i;
+
+	for (i = 9 - 1; i >= 0; i--)
+	{
+		putchar(s[i]);
+	}
+	putchar('\n');
+}
