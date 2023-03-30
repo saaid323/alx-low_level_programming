f04d90debe13674822468ed518447fe5c8631d35
saaid <saaid2538@gmail.com>
2023-03-30 08:31:57 -0700

    added 101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_numbers.c
+++ 0x06-pointers_arrays_strings/101-print_numbers.c
@@ -16,26 +16,7 @@ void print_number(int n)
 	}
 	if (n >= 0)
 	{
-		if (n > 10 && n < 100)
-		{
-			_putchar(n / 10 + '0');
-		}
-		if (n > 100 && n < 1000)
-		{
-			int m = n / 10;
-
-			_putchar(n / 100 + '0');
-			_putchar(m % 10 + '0');
-		}
-		if (n > 1000)
-		{
-			int m = n / 100;
-			int k = n / 10;
-
-			_putchar(m / 10 + '0');
-			_putchar(m / 100 + '0');
-			_putchar(k % 10 + '0');
-		}
-		_putchar(n % 10 + '0');
+		print_number(n / 10);
 	}
+	_putchar(n % 10 + '0');
 }
