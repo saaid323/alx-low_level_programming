dcd4d1f443b0e8cd0e0127aedfb7d8eff956b624
saaid <saaid2538@gmail.com>
2023-03-30 05:42:05 -0700

    added 7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+
+
+/**
+ * leet - encodes a string into 1337
+ * @s: pointer to string
+ * Return: returns s
+ */
+
+char *leet(char *s)
+{
+	char upper[] = {"AELOT"};
+	char low[] = {"aelot"};
+	char ch[] = {"43071"};
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 5; j++)
+		{
+			if (s[i] == upper[j] || s[i] == low[j])
+			{
+				s[i] = ch[j];
+				break;
+			}
+		}
+	}
+	return (s);
+}
