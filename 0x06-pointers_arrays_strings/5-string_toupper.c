57ad3cbbc44c66aefa7a34c4d978cba7ff18ab64
saaid <saaid2538@gmail.com>
2023-03-30 02:10:23 -0700

    added 5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+
+
+/**
+ * string_toupper - changes all lowercase letters of a string to uppercase
+ * @s: pointer to char
+ * Return: Uppercase letter
+ */
+
+char *string_toupper(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{
+			s[i] -= 32;
+		}
+		i++;
+	}
+	return (s);
+}
