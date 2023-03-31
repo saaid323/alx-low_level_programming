ad24e4418c5e0bd16f4254207f50093af4e89b60
saaid <saaid2538@gmail.com>
2023-03-31 11:24:53 -0700

    added 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - Entry point
@@ -7,6 +7,7 @@
  */
 int main(void)
 {
-	putchar('and that piece of art is useful\" - Dora Korpar, 2015-10-19' + '0');
-	return (0);
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(STDERR_FILENO, str, sizeof(str) - 1);
+	return (1);
 }
