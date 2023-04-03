87170dd1ec81bcc6fd946ba89bd0f146fd5c1372
saaid <saaid2538@gmail.com>
2023-04-03 02:52:04 -0700

    added 5-strstr.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,27 @@
+/**
+ * _strstr- locates a substring
+ * @haystack: string to be scanned
+ * @needle: string to be searched with-in haystack string
+ * Return:  pointer to haystack
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		for (j = 0; needle[j] != '\0'; j++)
+		{
+			if (haystack[i] != needle[j])
+			{
+				break;
+			}
+			else
+			{
+				return (&haystack[i]);
+			}
+		}
+	}
+	return ('\0');
+}
