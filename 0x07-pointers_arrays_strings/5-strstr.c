b040330811715e82a3c401e869ddaf4cf8304417
saaid <saaid2538@gmail.com>
2023-04-03 04:43:45 -0700

    added 5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -9,9 +9,9 @@ char *_strstr(char *haystack, char *needle)
 {
 	int i, j;
 
-	for (i = 0; haystack[i] >= '\0'; i++)
+	for (i = 0; haystack[i] != '\0'; i++)
 	{
-		for (j = 0; needle[j] >= '\0'; j++)
+		for (j = 0; needle[j] != '\0'; j++)
 		{
 			if (haystack[i] != needle[j])
 			{
