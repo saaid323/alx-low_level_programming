e9e935dfa921f0be791b0a86c3a01a2b7675924f
saaid <saaid2538@gmail.com>
2023-04-03 04:53:29 -0700

    added 5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,3 +1,4 @@
+#include <string.h>
 /**
  * _strstr- locates a substring
  * @haystack: string to be scanned
@@ -8,10 +9,12 @@
 char *_strstr(char *haystack, char *needle)
 {
 	int i, j;
+	int size_hay = strlen(haystack);
+	int size_needle = strlen(needle);
 
-	for (i = 0; haystack[i] != '\0'; i++)
+	for (i = 0; i <=  size_hay; i++)
 	{
-		for (j = 0; needle[j] != '\0'; j++)
+		for (j = 0; j <= size_needle; j++)
 		{
 			if (haystack[i] != needle[j])
 			{
