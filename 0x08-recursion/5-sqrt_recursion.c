55772462be3f486b6e6ddf91e813417b16dacaf6
saaid <saaid2538@gmail.com>
2023-04-05 06:39:24 -0700

    added 5-sqrt_recursion.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,32 @@
+
+/**
+ * _sqrt - returns the natural square root of a number
+ * @i: int parameter
+ * @j: int parameter
+ * Return: if i does not have sqrt the function should return -1
+ * else it returns sqrt of i
+ */
+
+int _sqrt(int i, int j)
+{
+	if (i * i == j)
+		return (i);
+	if (i * i > j)
+		return (_sqrt(i - 1, j));
+	else if ((i * i) - (int) (i * i) == 0)
+		return (-1);
+}
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: int parameter
+ * Return: if n does not have sqrt the function should return -1
+ * else it returns sqrt of n
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n == 1)
+		return  (1);
+	return (_sqrt(n - 1, n));
+}
