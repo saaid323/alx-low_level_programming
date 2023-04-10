a03c6b8883266b5cdd6c9cec7e28e7e822e020f5
saaid <saaid2538@gmail.com>
2023-04-10 07:24:57 -0700

    added 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entery point
+ * @argc: int argument
+ * @argv: pointer to a string
+ * Return: return 0 Always (success)
+ */
+
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
