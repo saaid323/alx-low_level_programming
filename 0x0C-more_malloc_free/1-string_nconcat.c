67c624e48813b29bc99acac226b3cb665107089d
saaid <saaid2538@gmail.com>
2023-04-13 06:14:15 -0700

    added 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -35,7 +35,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	}
 	for (i = 0; i <= n; i++)
 	{
-		c[j + i] = s2[i];
+		c[strlen(s1) + i] = s2[i];
 	}
 	c[strlen(s1) + n] = '\0';
 	return (c);
