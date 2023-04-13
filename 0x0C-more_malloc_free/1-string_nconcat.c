1ccc4b1aa0fd208256f17e4e6b00fe0cc38377ca
saaid <saaid2538@gmail.com>
2023-04-13 05:24:10 -0700

    added 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -25,7 +25,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (n > x)
 		n = x;
 
-	c = malloc(sizeof(char) * j + n);
+	c = malloc(sizeof(char) * (j + n + 1));
 
 	if (c == NULL)
 		return (NULL);
