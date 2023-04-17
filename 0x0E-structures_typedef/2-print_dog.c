e2295875fbb836239f3dbe8afe72bf2ecf5d8a6f
saaid <saaid2538@gmail.com>
2023-04-17 04:34:36 -0700

    added 2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,7 +14,7 @@ void print_dog(struct dog *d)
 			printf("Name: (nil)\n");
 		else
 			printf("Name: %s\n", d->name);
-		if (d->age < 0)
+		if (d->age > 0)
 			printf("Age: %.1f\n", d->age);
 		else
 			printf("Age:  (nil)\n");
