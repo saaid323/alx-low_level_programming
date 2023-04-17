81fb5a6654ead9fd64dcb929eb5f4282070774fd
saaid <saaid2538@gmail.com>
2023-04-17 02:41:30 -0700

    added dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * struct dog - details about dog
+ * @name: the name of the dog
+ * @owner: owner of the dog
+ * @age: age of the dog
+ */
+
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+
+#endif
