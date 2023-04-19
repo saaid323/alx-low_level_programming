c26210407b4d082bb55f0441fd90efd125b86e8b
saaid <saaid2538@gmail.com>
2023-04-19 07:03:09 -0700

    added 2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -14,15 +14,18 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 	if (size <= 0)
 		return (-1);
-	for (i = 0; i < size; i++)
+	if (array != NULL && cmp != NULL)
 	{
-		if (n(array[i]))
+		for (i = 0; i < size; i++)
 		{
-			return (i);
-		}
-		if (i == size - 1 && !n(array[i]))
-		{
-			return (-1);
+			if (n(array[i]))
+			{
+				return (i);
+			}
+			if (i == size - 1 && !n(array[i]))
+			{
+				return (-1);
+			}
 		}
 	}
 	return (0);
