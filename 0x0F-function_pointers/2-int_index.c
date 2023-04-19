bbb0ca3481f8c9cd529049b8163c3a4f6d1d680f
saaid <saaid2538@gmail.com>
2023-04-19 07:23:18 -0700

    added 2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,7 +12,9 @@ int int_index(int *array, int size, int (*cmp)(int))
 	int i;
 	int (*n)(int) = cmp;
 
-	if (size > 0)
+	if (size <= 0)
+		return (-1);
+	if (array != NULL && cmp != NULL)
 	{
 		for (i = 0; i < size; i++)
 		{
