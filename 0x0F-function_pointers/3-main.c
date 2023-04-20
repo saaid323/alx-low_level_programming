1aa96d5c973f433779efe08c4138fb7b82e9d06c
saaid <saaid2538@gmail.com>
2023-04-19 19:46:20 -0700

    added 3-main.c, 3-op_functions.c, 3-get_op_func.c, 3-calc.h

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -11,7 +11,7 @@
   */
 int main(int argc, char *argv[])
 {
-	int (*oprt)(int, int);
+	int (*oper)(int, int);
 
 	if (argc != 4)
 	{
@@ -19,14 +19,14 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
-	oprt = get_op_func(argv[2]);
+	oper = get_op_func(argv[2]);
 
-	if (!oprt)
+	if (!oper)
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
-	printf("%d\n", oprt(atoi(argv[1]), atoi(argv[3])));
+	printf("%d\n", oper(atoi(argv[1]), atoi(argv[3])));
 	return (0);
 }
