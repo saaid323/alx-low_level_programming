0fe41ab674b90f3af05592da9edc939c27f43611
saaid <saaid2538@gmail.com>
2023-04-19 19:43:33 -0700

    added 3-main.c, 3-op_functions.c, 3-get_op_func.c, 3-calc.h

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,36 @@
+
+/**
+ * op_add - adds two number
+ * @a: first number
+ * @b:second number
+ * Return: returns addition of a and b
+ */
+int op_add(int a, int b) { return (a + b); }
+/**
+ * op_sub - subtracts two number
+ * @a: first number
+ * @b:second number
+ * Return: returns subtractionof a and b
+ */
+int op_sub(int a, int b) { return (a - b); }
+/**
+ * op_mul - multiplies two number
+ * @a: first number
+ * @b:second number
+ * Return: returns product of a and b
+ */
+int op_mul(int a, int b) { return (a * b); }
+/**
+ * op_div - divides two number
+ * @a: first number
+ * @b:second number
+ * Return: returns division of a and b
+ */
+int op_div(int a, int b) { return (a / b); }
+/**
+ * op_mod - module two number
+ * @a: first number
+ * @b:second number
+ * Return: returns remainder of the division of a and b
+ */
+int op_mod(int a, int b) { return (a % b); }
