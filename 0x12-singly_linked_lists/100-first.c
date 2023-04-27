eeb3a5b7db06eaf170129ff40cb7bcbc7f996d31
saaid <saaid2538@gmail.com>
2023-04-27 10:29:16 -0700

    added 100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
+/**
+ * print - apply attribute constructor to print before main
+ * is executed
+ */
+
 void print(void)__attribute__ ((constructor));
+
+/**
+ * print - prints
+ */
 
 void print(void)
 {
