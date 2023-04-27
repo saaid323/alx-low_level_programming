e9c1e396e35946c0011c91959844be516778c32e
saaid <saaid2538@gmail.com>
2023-04-27 10:08:01 -0700

    added 4-free_list.c

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -7,11 +7,11 @@
 
 void free_list(list_t *head)
 {
-	if(head != NULL)
+	if (head != NULL)
 	{
 		free_list(head->next);
-			if (head->str)
-				free(head->str);
+		if (head->str)
+			free(head->str);
 			free(head);
 	}
 }
