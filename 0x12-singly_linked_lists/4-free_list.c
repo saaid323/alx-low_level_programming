9b64f89774ec1d1c7893725cc5129f5816dcf65e
saaid <saaid2538@gmail.com>
2023-04-27 09:36:28 -0700

    added 4-free_list.c

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * free_list - frees a list_t list
+ * @head: pointer to list
+ */
+
+void free_list(list_t *head)
+{
+	if (head != NULL)
+	{
+		free_list(head->next);
+		if (head->str)
+			free(head->str);
+		free(head);
+	}
+}
