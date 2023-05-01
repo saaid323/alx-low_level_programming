968cc0b7b02ddd21ccfe5470a6cb2c5295bccb31
saaid <saaid2538@gmail.com>
2023-05-01 10:36:23 -0700

    added 6-pop_listint.c

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -3,15 +3,17 @@
 int pop_listint(listint_t **head)
 {
 	listint_t *old = NULL;
+	int data;
 
-	if (*head == NULL)
+	if (*head == NULL || head == NULL)
 		return 0;
 	else
 	{
 		old = *head;
-		*head = (*head)->next;
+		*head = old->next;
+		data = old->n;
 		free(old);
 		old = NULL;
 	}
-	return (*head)->n;
+	return data;
 }
