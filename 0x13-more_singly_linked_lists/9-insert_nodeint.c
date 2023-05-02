9f9eaa838c8f444083d8955e111a5bdb68dad7f4
saaid <saaid2538@gmail.com>
2023-05-02 04:53:09 -0700

    added 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -24,7 +24,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		*head = temp1;
 		return (temp1);
 	}
-	for (i = 0; i < idx - 1; i++)
+	for (i = 0; temp != NULL && i < idx - 1; i++)
 	{
 		temp = temp->next;
 	}
