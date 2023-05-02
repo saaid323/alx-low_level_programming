27b6513f2949d6ef45791ed108101146285dc5e3
saaid <saaid2538@gmail.com>
2023-05-02 02:58:00 -0700

    added 7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -12,13 +12,11 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	listint_t *temp = head;
 	unsigned int i;
 
-	index -= 1;
 	for (i = 0; i < index; i++)
 	{
+		if (temp == NULL)
+			return (NULL);
 		temp = temp->next;
 	}
-	if (temp == NULL)
-                return (NULL);
-	temp = temp->next;
 	return (temp);
 }
