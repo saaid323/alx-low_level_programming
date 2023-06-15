1b4f9fb538e85e9aa3dd48c88ea0fd562faeb7fe
saaid <saaid2538@gmail.com>
2023-06-15 06:42:54 -0700

    added 0-print_dlistint.c

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * print_dlistint - prints all the elements of a dlistint_t list
+ * @head: pointer to struct
+ * Return: the number of nodes
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	int count;
+
+	count = 0;
+	while(h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		count ++;
+	}
+	return count;
+}
