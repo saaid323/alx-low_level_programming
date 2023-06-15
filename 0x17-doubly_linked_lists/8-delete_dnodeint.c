54b129357301f6eab3ed89e3a0ba4ff42a005dfc
saaid <saaid2538@gmail.com>
2023-06-15 11:02:04 -0700

    added 8-delete_dnodeint.c

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -10,33 +10,32 @@
 
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	 dlistint_t *current;
-	 unsigned int count;
+	dlistint_t *current;
+	unsigned int count;
 
-	 if (head == NULL || *head == NULL)
-		 return (-1);
-
-	 current = *head;
-	 if (index == 0)
-	 {
-		 *head = (*head)->next;
-		 if (*head != NULL)
-			 (*head)->prev = NULL;
-		 free(current);
-		 return (1);
-	 }
-	 count = 0;
-	 while (count < index && current != NULL)
-	 {
-		 current = current->next;
-		 count++;
-	 }
-	 if (current == NULL)
-		 return (-1);
-	 if (current->prev != NULL)
-		 current->prev->next = current->next;
-	 if (current->next != NULL)
-		 current->next->prev = current->prev;
-	 free(current);
-	 return (1);
+	if (head == NULL || *head == NULL)
+		return (-1);
+	current = *head;
+	if (index == 0)
+	{
+		*head = (*head)->next;
+		if (*head != NULL)
+			(*head)->prev = NULL;
+		free(current);
+		return (1);
+	}
+	count = 0;
+	while (count < index && current != NULL)
+	{
+		current = current->next;
+		count++;
+	}
+	if (current == NULL)
+		return (-1);
+	if (current->prev != NULL)
+		current->prev->next = current->next;
+	if (current->next != NULL)
+		current->next->prev = current->prev;
+	free(current);
+	return (1);
 }
