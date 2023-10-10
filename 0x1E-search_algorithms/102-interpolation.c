8f0b8289450d53983515236e9ec0d481bfe233e3
saaid323 <saaid2538@gmail.com>
2023-10-10 13:49:35 -0700

    added 102-interpolation.c

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -11,7 +11,7 @@
 int interpolation_search(int *array, size_t size, int value)
 {
 	unsigned int l = 0, h = size - 1, pos;
-	
+
 	if (array == NULL)
 		return (-1);
 	while (array[l] != array[h] && value >= array[l] && value <= array[h])
