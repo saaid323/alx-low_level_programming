e709b1c07eea0617ba501b82b869c01beb4fc88e
saaid323 <saaid2538@gmail.com>
2023-10-10 03:52:10 -0700

    added 1-binary.c

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -12,7 +12,7 @@ int binary_search(int *array, size_t size, int value)
 {
 	size_t l = 0, r = size - 1, i;
 
-	if (array == NULL)
+	if (array == NULL || size == 0)
 		return (-1);
 	while (l < r)
 	{
